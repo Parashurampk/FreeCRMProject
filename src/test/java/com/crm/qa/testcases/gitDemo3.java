256b9458630e12665a348aeeee7c0024bc7e202c
Parashurampk <walikindi@gmai.com>
2022-06-26 00:36:55 +0530

    pushing to developer branch

---
+++ src/test/java/com/crm/qa/testcases/gitDemo3.java
@@ -0,0 +1,18 @@
+package com.crm.qa.testcases;
+
+public class gitDemo3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hey , i am software Engineer");
+		System.out.println("Hey , i am software Engineer");
+		System.out.println("Hey , i am software Engineer");
+		System.out.println("Hey , i am software Engineer");
+		System.out.println("Hey , i am software Engineer");
+		System.out.println("Hey , i am software Engineer");
+		
+		
+	
+	}
+
+}
