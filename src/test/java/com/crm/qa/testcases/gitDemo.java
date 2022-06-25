148cd591dccd1df0cdd52810e333aca0022acfee
Parashurampk <80196930+Parashurampk@users.noreply.github.com>
2022-06-25 23:55:16 +0530

    Update gitDemo.java

--- src/test/java/com/crm/qa/testcases/gitDemo.java
+++ src/test/java/com/crm/qa/testcases/gitDemo.java
@@ -5,6 +5,9 @@ public class gitDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("I am Automation engineer");
+		System.out.println("I am Automation engineer");
+		System.out.println("I am Automation engineer");
+		System.out.println("I am Automation engineer");
 	}
 
 }
